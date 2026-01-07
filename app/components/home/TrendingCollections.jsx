62a5158604ac4df9311fd2ea9400c2e2d05c1a63
forsyth01 <forsythokoeguale01@gmail.com>
2026-01-07 19:24:34 +0100

    new commit

--- app/components/home/TrendingCollections.jsx
+++ app/components/home/TrendingCollections.jsx
@@ -3,11 +3,14 @@ import ProductSwiper from "../ProductSwiper";
 import ProductCard from "../product/ProductCard";
 
 export default function TrendingCollections() {
+  // Only take the first 4 products
+  const trendingProducts = products.slice(0, 4);
+
   return (
     <ProductSwiper
       eyebrow="SETPIECE"
       title="TRENDY COLLECTIONS"
-      products={products}
+      products={trendingProducts}
       ProductCard={ProductCard}
       viewAllText="Want more of this?"
       viewAllHref="/collections/trendy"
