32546b4859e8fa44f1294ddb61cd5d9e3733d402
forsyth01 <forsythokoeguale01@gmail.com>
2026-03-13 00:04:19 +0100

    new commit

--- app/account/forgot-password/page.jsx
+++ app/account/forgot-password/page.jsx
@@ -1,13 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-
-const SHOPIFY_ACCOUNTS_URL = "https://accounts.setpiecesclothing.com";
+import { redirect } from "next/navigation";
 
 export default function ForgotPasswordPage() {
-  useEffect(() => {
-    window.location.replace(SHOPIFY_ACCOUNTS_URL);
-  }, []);
-
-  return null;
+  redirect("https://accounts.setpiecesclothing.com");
 }
